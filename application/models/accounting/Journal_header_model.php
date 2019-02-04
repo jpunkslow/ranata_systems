f629756e2ff167958c3489f1cfdf31a09833f044
Dian Nugraha <nugashare@gmail.com>
2019-02-04 14:33:06 +0700

    journal header

--- application/models/accounting/Journal_header_model.php
+++ application/models/accounting/Journal_header_model.php
@@ -29,4 +29,11 @@ class Journal_header_model extends Crud_model {
         return $data;
     }
 
+    function triggerDelete($id){
+
+        $data = $this->db->query("UPDATE transaction_journal SET deleted = 1 WHERE fid_header = '$id'");
+
+        return $data;
+    }
+
 }
